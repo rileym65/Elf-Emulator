517fd56b89f484d791f469896ef2313b226f5ebe
Mike Riley <mikehriley@hotmail.com>
2020-11-14 12:22:34 -0800

    Fixed librcs.h include in toggle.c to use included librcs.h

--- toggle.c
+++ toggle.c
@@ -8,7 +8,7 @@
  *******************************************************************
 */
 
-#include <librcs.h>
+#include "librcs.h"
 #include "elf.h"
 #include "images/swup.xpm"
 #include "images/swdn.xpm"
